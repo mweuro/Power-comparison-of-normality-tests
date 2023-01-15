15fda4b20196242d99dfe4cd3f5e9ad53afc361b
Michał <86123830+mweuro@users.noreply.github.com>
2023-01-15 21:43:20 +0100

    Plots powers but something is wrong with KS-test

--- power_comparison.r
+++ power_comparison.r
@@ -1,21 +1,9 @@
 library(nortest)
+library(ggplot2)
+library(purrr)
 
-#Finds critical value of test based on
-#50 000 samples of 100 independent observations
-normal_crit <- function(test_fun, alpha, N = 50000, n = 100){
-  X <- replicate(N, rnorm(n, mean = 0, sd = 1))
-  dim(X) <- c(n, N)
-  T <- apply(X, 2, test_fun)
-  
-  if(test_fun == 'sw'){
-    crit = quantile(T, alpha)
-  }
-  else{
-    crit = quantile(T, 1 - alpha)
-  }
-  
-  return(crit)
-}
+
+#TESTS' STATISTICS
 
 #Kolmogorov_Smirnov test statistic
 ks <- function(x){
@@ -34,6 +22,106 @@ lillie <- function(x){
 
 #Anderson-Darling test statistic
 ad <- function(x){
-  return(ad.test(x)$statistic)
+  stat <- ad.test(x)$statistic
+  n <- length(x)
+  return(stat*(1 + 0.75*n + 2.25*n^2))
 }
+
+
+# #DISTRIBUTIONS' SAMPLES
+# 
+# #Uniform
+# u_sample <- function(n, a, b){
+#   return(runif(n, a, b))
+# }
+# 
+# #Beta
+# beta_sample <- function(n, a, b){
+#   return(rbeta(n, a, b))
+# }
+# 
+# #t
+# t_sample <- function(n, df){
+#   return(rt(n, df))
+# }
+# 
+# #Laplace
+# laplace_sample <- function(n){
+#   return(rlaplace(n))
+# }
+# 
+# #Chi2
+# chi2_sample <- function(n, df){
+#   return(rchisq(n, df))
+# }
+# 
+# #Gamma
+# gamma_sample <- function(n, shape, scale){
+#   return(rgamma(n, shape = shape, scale = scale))
+# }
+
+
+#Finds critical value of test based on
+#50 000 samples of 100 independent observations
+test_power <- function(test_fun, alpha, N = 5000, M = 500){
+  test_name <- as.character(substitute(test_fun))
+  p <- c()
+  n <- c(10, 20, 30, 50, 100, 200, 300, 400, 500, 1000, 2000)
+  crits <- c()
+  for(i in n){
+    X <- replicate(N, rnorm(i, mean = 0, sd = 1))
+    dim(X) <- c(i, N)
+    T <- apply(X, 2, test_fun)
+    
+    if(test_name == 'sw'){
+      crit <- quantile(sort(T), alpha)
+    }
+    else if(test_name == 'ks'){
+      crit <- quantile(sort(T), 1 - alpha/2)
+    }
+    else{
+      crit <- quantile(sort(T), 1 - alpha)
+    }
+    crits <- append(crits, crit)
+  }
+
+  for(i in 1:length(n)){
+    Y <- replicate(M, rgamma(n[i], 1, 5))
+    dim(Y) <- c(n[i], M)
+    D <- apply(Y, 2, test_fun)
+    if(test_name == 'sw'){
+      d <- length(which(D < crits[i]))
+    }
+    else{
+      d <- length(which(D > crits[i]))
+    }
+    p <- append(p, d/M)
+  }
+  return(p)
+}
+
+plot_powers <- function(alpha){
+  n <- c(10, 20, 30, 50, 100, 200, 300, 400, 500, 1000, 2000)
+  p_sw <- test_power(sw, alpha)
+  p_ks <- test_power(ks, alpha)
+  p_lillie <- test_power(lillie, alpha)
+  p_ad <- test_power(ad, alpha)
+  df <- data.frame(n, p_sw, p_ks, p_lillie, p_ad)
+  
+  ggplot(df, aes(x = n)) + 
+    geom_line(y = p_sw, color = 'blue') +
+    geom_point(y = p_sw, shape = 18, color = 'blue') +
+    geom_line(y = p_ks, color = 'red') +
+    geom_point(y = p_ks, shape = 15, color = 'red') +
+    geom_line(y = p_lillie, color = 'green') +
+    geom_point(y = p_lillie, shape = 17, color = 'green') +
+    geom_line(y = p_ad, color = 'purple') +
+    geom_point(y = p_ad, shape = 4, color = 'purple') +
+    scale_x_continuous(breaks = c(10, 30, 100, 300, 1000, 2000),
+                       trans = 'log2') +
+    scale_y_continuous(breaks = seq(0, 1, by = 0.1)) +
+    theme_classic() + 
+    labs(x = 'Sample size, n', y = 'Simulated Power')
+}
+plot_powers(0.05)
 
